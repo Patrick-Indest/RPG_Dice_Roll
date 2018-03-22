5ea13a258b08d4940b051bd91165a6a46df17dc2
Patrick-Indest <37672280+Patrick-Indest@users.noreply.github.com>
2018-03-22 14:54:57 -0500

    Adding base Python Script
    Linking up with cloud for safe storage.

---
+++ DiceRoll.py
@@ -0,0 +1,24 @@
+import random
+
+# User options. User picks dice. Valid responses for side are 4,6,8,10,12,20,100 and the number of dice 1 to 100.
+diceSide = raw_input("What sided dice would you like to roll? (Valid dice are: 4,6,8,10,12,20,100)")
+	if diceSide == "4"
+		then [ random.randint(1, 4) for i in range(howMany)
+	if diceSide == "6"
+		then [ random.randint(1, 6) for i in range(howMany)
+	if diceSide == "8"
+		then [ random.randint(1, 8) for i in range(howMany)
+	if diceSide == "10"
+		then [ random.randint(1, 10) for i in range(howMany)
+	if diceSide == "12"
+		then [ random.randint(1, 12) for i in range(howMany)
+	if diceSide == "20"
+		then [ random.randint(1, 20) for i in range(howMany)
+	if diceSide == "100"
+		then [ random.randint(1, 100) for i in range(howMany)
+	
+howMany = raw_input("How many dice would you like to roll? (Valid number of dice are: 1-100)")
+
+
+# User has the Option to roll again. 
+rollAgain = raw_input("Would you like to roll again (Yes or No)?")
